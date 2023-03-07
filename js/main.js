ac6d91d649f27f668603f88dfd5ed2589ad292d7
Simone Viglione <zimoviglio51@gmail.com>
2023-03-07 18:27:16 +0100

    implementata prima timeout

--- js/main.js
+++ js/main.js
@@ -1,23 +1,33 @@
 function generaNumeri(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
-  }
+}
   
-  let numeri = [];
+let numeri = [];
   
-  while (numeri.length < 5) {
-    const numeroCasuale = generaNumeri(1, 100);
-    if (!numeri.includes(numeroCasuale)) {
-      numeri.push(numeroCasuale);
+while (numeri.length < 5) {
+
+const numeroCasuale = generaNumeri(1, 100);
+
+if (!numeri.includes(numeroCasuale)) {
+    numeri.push(numeroCasuale);
     }
-  }
+}
   
-  const ul = document.getElementById("numeriRandom");
+const ul = document.getElementById("numeriRandom");
   
-  numeri.forEach(numero => {
-    const li = document.createElement("li");
-    li.textContent = numero;
-    ul.appendChild(li);
-  });
+numeri.forEach(numero => {
+const li = document.createElement("li");
+li.textContent = numero;
+ul.append(li);
+});
+  
+setTimeout(() => {
+const lis = document.querySelectorAll("#numeriRandom li");
+
+lis.forEach(li => li.classList.add("d-none"));
+}, 30000);
+  
+
   
   
   
